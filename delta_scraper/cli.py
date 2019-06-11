a5f472b82d6d9b0891cd216ee6fad5cda0f6a296
Simon Willison <simonw@eventbrite.com>
2019-06-11 08:18:15 -0700

    Applied black

--- delta_scraper/cli.py
+++ delta_scraper/cli.py
@@ -13,7 +13,7 @@ def cli():
     "path",
     type=click.Path(exists=True, file_okay=True, dir_okay=False, allow_dash=False),
     required=False,
-    default="."
+    default=".",
 )
 def run(path):
     """Run all scraper classes in the specified directory"""
