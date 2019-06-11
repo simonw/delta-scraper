91ad612db22b5a76a5ed12fee406feeb9c30c378
Simon Willison <simonw@eventbrite.com>
2019-06-11 08:18:03 -0700

    First basic test

--- tests/test_delta_scraper.py
+++ tests/test_delta_scraper.py
@@ -1,2 +1,13 @@
-def test_delta_scraper():
-    assert False, "No tests yet"
+from delta_scraper import DeltaScraper
+
+
+def test_delta_scraper(capsys):
+    class Foo(DeltaScraper):
+        def fetch_data(self):
+            return {"hello": "world"}
+
+    foo = Foo(None)
+    foo.test_mode = True
+    assert None is foo.scrape_and_store()
+    captured = capsys.readouterr()
+    assert '{\n  "hello": "world"\n}\n' == captured.out
